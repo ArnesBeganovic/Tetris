fae78f456be2e88399f86732c9b2e255cea41ecf
Arnes Beganovic <arnesbeganovic@gmail.com>
2017-03-04 09:25:24 +0100

    Working version

--- Tetris/main.cpp
+++ Tetris/main.cpp
@@ -51,17 +51,20 @@ int main()
 	
 
 	//Render window - work place
-	RenderWindow window(VideoMode(329, 480), "The Game!");
+	RenderWindow window(VideoMode(320, 480), "Tetris!");
 
 	//Load image from folder
-	Texture t;
-	t.loadFromFile("../Images/tiles.png");
+	Texture t1,t2,t3;
+	t1.loadFromFile("../Images/tiles.png");
+	t2.loadFromFile("../Images/background.png");
+	t3.loadFromFile("../Images/frame.png");
 
 	//Get one sprite from image
-	Sprite s(t);
+	Sprite s(t1),background(t2), frame(t3);
+
 	s.setTextureRect(IntRect(0, 0, 18, 18));
 
-	//controll rotation and left/right movements
+	//initial rotation and left/right movements
 	int dx = 0; bool rotate = 0; int colorNum = 1;
 
 	float timer = 0, delay=0.3;
@@ -103,13 +106,17 @@ int main()
 
 		}
 
+		if (Keyboard::isKeyPressed(Keyboard::Down)) {
+			delay = 0.05;
+		}
 		// Move tile after event keypress. Initially it is 0 and after key press it is changed +-1
 		for (int i = 0; i < 4; i++) {
 			b[i] = a[i];
 			a[i].x = a[i].x + dx;
 		}
 
-
+		//Now check position of tile in new frame. If return false than move back to b[i] position. 
+		//It will return false when x=-1,x=>M, y>=N or field array is already populated at that place
 		if (!check()) {
 			for (int i = 0; i < 4; i++) {
 				a[i] = b[i];
@@ -120,6 +127,7 @@ int main()
 		if (rotate == true) {
 			Point p = a[1];//center of rotation
 			for (int i = 0; i < 4; i++) {
+				//Move each tile clockwise where x possition is 
 				int x = a[i].y - p.y;
 				int y = a[i].x - p.x;
 				a[i].x = p.x - x;
@@ -155,33 +163,54 @@ int main()
 			timer = 0;
 		}
 
-
+		//check lines
+		int k = M - 1;
+		for (int i = M - 1; i > 0; i--) {
+			int count = 0;
+			for (int j = 0; j < N; j++) {
+				if (field[i][j]) {
+					count= count +1;
+				}
+				/*
+				if count is less then N it means not all columns are populated in one row
+				in that case, copy same value. but if count=N then whole row is populated
+				and copy field above. THis will effectivly erase row which is populated
+				*/
+				field[k][j] = field[i][j];
+			}
+			if (count < N) {
+				k--;
+			}
+		}
 		
 
 		//nuliraj pomjeranje i rotaciju za iduci krug
-		dx = 0; rotate = 0;
+		dx = 0; rotate = 0; delay = 0.3;
 		//make window white background
 		window.clear(Color::White);
+		window.draw(background);
 
-
+		//Show them when they reach end
 		for (int i = 0; i < M; i++) {
 			for (int j = 0; j < N; j++) {
 				if (field[i][j] == 0) {
 					continue;
 				}
-				s.setPosition(j * 18, i * 18);
+				s.setTextureRect(IntRect(field[i][j] * 18, 0, 18, 18));
+				s.setPosition((j * 18)+28, (i * 18)+31);
 				window.draw(s);
 			}
 		}
 
 		//show 4 tiles based on struckt
 		for (int i = 0; i < 4; i++) {
-			s.setPosition(a[i].x * 18, a[i].y * 18);
+			s.setTextureRect(IntRect(colorNum * 18, 0, 18, 18));
+			s.setPosition((a[i].x * 18)+28, (a[i].y * 18)+31);
 			window.draw(s);
 		}
 		window.display();
 	}
-	
 	return 0;
+	//End
 }
 
